c90bd55911d085c26d552b5232616eee6fd48cfa
dulceeee <79719284+dulceeee@users.noreply.github.com>
2022-07-20 09:21:51 -0600

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,15 +1,5 @@
 import {user_ob} from './usuarios.js';
-
-let user = [
-    [100,	'anna0',	    'white00',	    'Anna Addison',	    '1325 Candy Rd, San Francisco, CA 96123',	'anna.addison@yahoo.com',	3841019535],
-    [101,	'carol1',	    'orange01',	    'Carol Campbell',	'1931 Brown St, Gainesville, FL 85321',	    'carol.campbell@aol.com', 	2548952651],
-    [102,	'julia2',	    'yellow02',	    'Julia Jones',	    '1622 Seaside St, Seattle, WA 32569',	    'jolie.jones@msn.com',	    3651469235],
-    [103,	'irene3',	    'blue03',	    'Irene Everly',	    '1756 East Dr, Houston, TX 28562',	        'irene.everly@gmail.com',	9859423698],
-    [104,	'rachel4',	    'red04',	    'Rachel Rose',	    '1465 River Dr, Boston, MA 43625',	        'rachel.rose@hotmail.com',	2945632543],
-    [105,	'sophie5',	    'green05',	    'Sophie Sutton',	'1896 West Dr, Portland, OR 65842',	        'sophie.sutton@yahoo.com',	5169525614],
-    [106,	'wendy6',	    'purple06',	    'Wendy West',	    '1252 Vine St, Chicago, IL 73215',	        'wendy.west@gmail.com', 	6645936156],
-    [106,	'jefuentes',    'gatitos123',   'Jossue Fuentes',   'Colegio Santa Catalina SCL',	            'jefuentes@scl.edu.gt', 	4568443548]
-]
+import {user} from './user_arrive.js'
 
 let caja_texto = document.querySelector("#caja_texto");
 let caja_usuario = document.querySelector('#root')
@@ -42,4 +32,12 @@ bt_siguiente.addEventListener('click', boton_siguiente)
 bt_anterior.addEventListener('click', boton_anterior)
 
 //INERT HTML
-caja_usuario.innerHTML = '<h1>' + user_ob[0].name + '<h1>';
+function mostrar(){
+    caja_usuario.innerHTML = '<h1>' + user_ob[nameUser].nickname +'<h1>'+
+    '<h1>' + user_ob[nameUser].name + '</h1>'+
+    '<h1>' + user_ob[nameUser].address + '</h1>'+
+    '<h1>' + user_ob[nameUser].email + '</h1>'+
+    '<h1>' + user_ob[nameUser].phone_number + '</h1>';
+}
+
+bt_user.addEventListener('click', mostrar);
